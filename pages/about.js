385a833a96f4ddbb9b2c8aa6bd9cbd6abcebe69c
Tectumor G <tectumor@Tectumors-MacBook-Pro.local>
2021-05-08 12:40:57 -0500

    second commit - add page titles

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,13 @@
+import Layout from "@components/Layout";
+
+
 export default function AboutPage() {
     return (
-        <div>
+        <Layout title="DJ Events | About">
             <h1>About</h1>
             <p>This is an app to find the latest DJ and other musical events</p>
             <p>Version: 1.0.0</p>
-        </div>
+        </Layout>
     )
 }
 
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,15 @@
+import { useRouter } from "next/Router";
+import Layout from "@components/Layout";
+
 export default function EventPage() {
+    const router = useRouter();
+    console.log(router);
+
     return (
-        <div>
+        <Layout>
             <h1>My Event</h1>
-        </div>
+            <p>{router.query.slug}</p>
+            <button onClick={() => router.push("/")}>Click</button>
+        </Layout>
     )
 }
